42ea3142d35ececcf550decbfabeacf56aa4912a
Neil Suji <63812857+neils94@users.noreply.github.com>
2022-04-18 11:33:34 -0700

    Update "Model".py

--- "Model".py
+++ "Model".py
@@ -1,9 +1,9 @@
 "Model"
 
-import tensorflow as tf 
-import tensorflow.layers as tfl
+import torch 
+from torch import nn
+import torch.functional as F
 import numpy as np
-from tensorflow.keras.layers import Activations
 import matplotlib.pyplot as plt
 
 
@@ -22,26 +22,25 @@ class Actor(nn.Module):
     def __init__(self, state_size, action_size, hidden_layer, hidden2_layer, seed):
         
         super(Actor, self).__init__()
-        self.fc1 = tfl.layers.Linear(num_input_dim=state_size, units=hidden_layer)
-        self.fc2 = tfl.layers.Linear(num_input_dim=state_size, units=hidden_layer)
-        self.fc3 = tfl.layers.Linear(num_input_dim=state_size, units=hidden_layer)
-        self.fc4 = tfl.layers.Linear(num_input_dim=state_size, units=hidden_layer)
-        self.fc5 = tfl.layers.Linear(num_input_dim=state_size, units=hidden_layer)
-        self.fc6 = tfl.layers.Linear(num_input_dim=state_size, units=action_size)
-        self.batch_norm = tf.keras.layers.BatchNormalization(axis=1)(state_size)
+        self.fc1 = torch.nn.Linear(state_size, hidden_layer)
+        self.fc2 = torch.nn.Linear(hidden_layer, hidden2_layer)
+        self.fc3 = torch.nn.Linear(hidden_layer, hidden2_layer)
+        self.fc4 = torch.nn.Linear(hidden_layer, hidden2_layer)
+        self.fc5 = torch.nn.Linear(hidden_layer, hidden2_layer)
+        self.fc6 = torch.nn.Linear(hidden2_layer, action_size)
+        
+        self.batch_norm =torch.nn.BatchNorm1d(state_size)
            
     def forward(self, states):
         if states.dim() == 1:
-            states = tf.expand_dims(states, 0)
+            states = torch.unsqueeze(states, 0)
         states = self.batch_norm(states)
         
-        relu = tensorflow.keras.layers.Activations('relu')
-        x = relu(self.fc1(self.fc1))
-        x = relu(self.fc2(x))
-        x = relu(self.fc3(x))
-        x = relu(self.fc4(x))
-        x = relu(self.fc5(x))
-        x = relu(self.fc1(x))
+        x = F.relu(self.fc1(self.fc1))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
         
         
         
@@ -53,28 +52,26 @@ class Critic(nn.Module):
         
         
         super(Critic,self).__init__()
-        self.fc1 = tfl.layers.Linear(num_input_dim=state_size, units=hidden_layer)
-        self.fc2 = tfl.layers.Linear(num_input_dim=hidden_layer, units=hidden_layer2)
-        self.fc3 = tfl.layers.Linear(num_input_dim=hidden_layer, units=hidden_layer2)
-        self.fc4 = tfl.layers.Linear(num_input_dim=hidden_layer, units=hidden_layer2)
-        self.fc5 = tfl.layers.Linear(num_input_dim=hidden_layer, units=hidden_layer2)
-        self.fc6 = tfl.layers.Linear(num_input_dim=hidden_layer, units=action_size)
+        self.fc1 = torch.nn.Linear(state_size, hidden_layer)
+        self.fc2 = torch.nn.Linear(hidden_layer, hidden2_layer)
+        self.fc3 = torch.nn.Linear(hidden_layer, hidden2_layer)
+        self.fc4 = torch.nn.Linear(hidden_layer, hidden2_layer)
+        self.fc5 = torch.nn.Linear(hidden_layer, hidden2_layer)
+        self.fc6 = torch.nn.Linear(hidden2_layer, action_size)
         
-        self.batch_norm = tf.keras.layers.BatchNormalization(axis=1)(state_size)
+        self.batch_norm = torch.nn.BatchNorm1d(state_size)
         
     
     def forward(self, states):
         if states.dim() == 1:
-            states = tf.expand_dims(states, 0)
+            states = torch.unsqueeze(states, 0)
             
         states = self.batch_norm(states)
 
-        relu = tensorflow.keras.layers.Activations('relu')
-        xs = relu(self.fc1(self.fc1))
-        x = relu(self.fc2(xs))
-        x = relu(self.fc3(x))
-        x = relu(self.fc4(x))
-        x = relu(self.fc5(x))
-        x = relu(self.fc1(x))
+        xs = F.relu(self.fc1(self.fc1))
+        x = F.relu(self.fc2(xs))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
         
         return self.fc6(x)
