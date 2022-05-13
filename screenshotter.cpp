96a0e5f2971cc1377f8fe1376f04a6e2afc77839
Neil Suji <63812857+neils94@users.noreply.github.com>
2022-05-13 15:50:31 -0700

    Add files via upload

---
+++ screenshotter.cpp
@@ -0,0 +1,14 @@
+#include <thread>
+#include <stdlib.h>
+#include <string>
+#include <ctime>
+#include <unistd.h>
+#include <time.h>
+
+#define UTC (3)
+
+using namespace std;
+
+struct tm * ptm;
+
+cout << ptm << endl;
