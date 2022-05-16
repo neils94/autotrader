9f60e564542fca2768d9adbd7be761d4d0f898f3
Neil Suji <63812857+neils94@users.noreply.github.com>
2022-05-16 15:22:46 -0700

    Add files via upload

---
+++ pythonscreenshotter.py
@@ -0,0 +1,20 @@
+import pyautogui
+import datetime
+import time
+
+def screenshot():
+    path_to_save_to = "/Users/neilsuji/Downloads/screenshot_images/"
+    screenshot_taken = pyautogui.screenshot(path_to_save_to+str(datetime.datetime.now()) + ".png")
+
+for i in range(1):
+
+    while True:
+        time.sleep(60 - time.gmtime()[5] % 60)
+        screenshot()
+
+        break;
+
+
+
+
+
