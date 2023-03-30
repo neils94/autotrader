5d2cf41446b99a25d2466ce20ab3017c182a82d2
Neil Suji <63812857+neils94@users.noreply.github.com>
2023-03-29 21:00:29 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 from contextlib import nullcontext
 from AdvantageAgent import AC_agent
-from Environment import environment
-from Model import Actor, Critic
+from meta_env2 import meta_env2
 import time
+import pyautogui
+import torch
+import sys
+import wandb
+import requests
+import Agent2
 
-
-alpha = 0.99
-beta = 0.01
 eps= 0.9
 gamma = 0.99
 lamb = 1e-2
@@ -16,35 +18,49 @@ state= nullcontext
 lr = 1e-4
 wd = 1e-4
 buffer_size = int(1e5)
-batch_size = 64
+batch_size = 128
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-env = environment()
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
 
-agent = AC_agent(batch_size, buffer_size, state, linear_input=2592, action_size=2, hidden_layer=256, hidden2_layer=256, input_channels=1, input_channels2=16, output_channels=16, output_channels2=32, 
+
+sys.stdout = Unbuffered(sys.stdout)
+
+agent = AC_agent(batch_size, buffer_size, state, linear_input=2592, action_size=2, hidden_layer=256, hidden2_layer=256, input_channels=4, input_channels2=16, output_channels=16, output_channels2=32, 
     kernel_size=8, kernel_size2=4, stride=4, stride2=2)
+env = meta_env2()
 
-def train_advantage(trading_week, trading_day):
-
+agent2 = Agent2(batch_size, buffer_size, state, linear_input=2592, action_size=2, hidden_layer=256, hidden2_layer=256, input_channels=4, input_channels2=16, output_channels=16, output_channels2=32, 
+    kernel_size=8, kernel_size2=4, stride=4, stride2=2)
+def forward_advantage():
+    closed = []
     scores = []
-    for i in range(trading_week):
-        state = env.screenshot()
-        if time.gmtime()[3] == 21:
-            sleeptime = (60 % time.gmtime()[4]) * 60 + time.gmtime()[5]
-            time.sleep(3600 - sleeptime % 3600) 
-
-        for i in range(trading_day):
-            action_probs, action = agent.act(state=state)
-            reward, next_state, close = env.step(action_probs, action)
-            assert close == 1
-            agent.step(state=state, action=action, action_probs=action_probs, reward=reward, next_state=next_state, close=close)
+    state = env.stacked_frames()
+    status_code = int(200)
+    while ((env.account_info()).status_code == status_code):
+            time.sleep(60 - time.gmtime()[5] % 60)
+            action_combo = agent.greedy_action(state)
+            action_probs, action_1 = action_combo
+            reward, action_2, next_state_1, next_state_2 = env.step(action_probs, action_1)
+            assert action_2 == 1
+            close = True
+            closed.append(close)
+            agent.memory_update(state, action_1, action_2, action_probs, reward, next_state_1, next_state_2, close)
+            state = next_state_2
             scores.append(reward)
-            state = next_state
-        
+            pyautogui.press('shift')
 
-
+    else:
+        print(env.account_info().status_code)
 
     return scores
-
-scores = train_advantage(trading_week=1, trading_day=50)
-
-print(scores)
