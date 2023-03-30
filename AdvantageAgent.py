6f49ba4a8d65bf5138222d1573def1a9e0a7fe17
Neil Suji <63812857+neils94@users.noreply.github.com>
2023-03-29 20:52:31 -0700

    Update AdvantageAgent.py

--- AdvantageAgent.py
+++ AdvantageAgent.py
@@ -1,15 +1,18 @@
-from Model import Actor, Critic, DQN
+from re import I
+from Model import Actor, Critic
 import random
 import torch
 import torch.nn.functional as F
 from torch import optim
-import ReplayBuffer
 import numpy as np
 from numpy import random
 import torch.nn
 from ReplayBuffer import ReplayBuffer
 import torch.multiprocessing
-from Environment import environment
+from scipy import stats
+from meta_API_environment import meta_env
+import heapq
+import wandb
 
 alpha = 0.99
 beta = 0.01
@@ -22,7 +25,7 @@ tau = 1e-3
 lr = 1e-4
 wd = 1e-4
 buffer_size = int(1e5)
-batch_size = 64
+batch_size = 128
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -36,27 +39,43 @@ class AC_agent():
                             kernel_size, kernel_size2, stride, stride2):
         self.Actor = Actor(state, linear_input, action_size, hidden_layer, hidden2_layer, input_channels, input_channels2, output_channels, output_channels2, 
                             kernel_size, kernel_size2, stride, stride2)
+        self.Actor_target = Actor(state, linear_input, action_size, hidden_layer, hidden2_layer, input_channels, input_channels2, output_channels, output_channels2, 
+                            kernel_size, kernel_size2, stride, stride2)
         self.Critic = Critic(state, linear_input, action_size, hidden_layer, hidden2_layer, input_channels, input_channels2, output_channels, output_channels2, 
                             kernel_size, kernel_size2, stride, stride2)
         self.Critic_target = Critic(state,linear_input,action_size, hidden_layer, hidden2_layer, input_channels, input_channels2, output_channels, output_channels2, 
                             kernel_size, kernel_size2, stride, stride2)
-        self.Actor_target = Actor(state,linear_input, action_size, hidden_layer, hidden2_layer, input_channels, input_channels2, output_channels, output_channels2, 
+        self.Actor_2 = Critic(state,linear_input, action_size, hidden_layer, hidden2_layer, input_channels, input_channels2, output_channels, output_channels2, 
                             kernel_size, kernel_size2, stride, stride2)
         self.Actor_optimizer = optim.Adam(self.Actor.parameters(), lr=lr, eps=eps, weight_decay=wd)
         self.Critic_optimizer = optim.Adam(self.Critic.parameters(), lr=lr, eps=eps, weight_decay=wd)
-        self.DQN = DQN(state, linear_input, action_size, hidden_layer, hidden2_layer, input_channels, input_channels2, output_channels, output_channels2, 
-                            kernel_size, kernel_size2, stride, stride2)
         self.memory = ReplayBuffer(buffer_size, batch_size)
         self.batch_size = batch_size
         self.buffer_size = buffer_size
         self.t_step = 0
-        self.env = environment()
+        self.env = meta_env()
     
-    def act(self, state):
-        state = self.env.preprocess(state)
+    def greedy_action(self, state):
         self.Actor.eval()
         with torch.no_grad():
-            action_probs, action = torch.max(self.Actor(state), dim=-1, keepdim=False, out=None)
+            action_probs, action = torch.max(self.Actor(state).cpu(), dim=-1, keepdim=False, out=None)
+        self.Actor.train()
+        return action_probs, action
+
+    def entropy(self, action_prob):
+        entropy = stats.entropy(action_prob)
+
+        return entropy
+
+    def entropy_action(self, state, exp = 0.9):
+        self.Actor.eval()
+        if random.random() > exp:
+            
+            with torch.no_grad():
+                action_probs, action = torch.min(self.Actor(state).cpu(), dim=-1, keepdim=False, out=None)
+        else:
+            with torch.no_grad():
+                action_probs, action = torch.max(self.Actor(state).cpu(), dim=-1, keepdim=False, out=None)
         self.Actor.train()
         return action_probs, action
 
@@ -65,20 +84,35 @@ class AC_agent():
         self.memory.add(state, action, action_probs, reward, next_state, close)
 
         t_step = self.t_step % update_every
-
         if t_step == 0:
+            
             if len(self.memory) > batch_size:
 
                 experience = self.memory.sample()
                 self.learn(experience, gamma)
-            if len(self.memory) % 500:
-                self.slow_updates(self.Actor, self.Actor_target, self.Critic, self.Critic_target)
+                if len(self.memory) % 500 == 0:
+                    self.slow_updates(self.Actor, self.Actor_target, self.Critic, self.Critic_target)
 
+    def memory_update(self, state, action, action_probs, reward, next_state, close):
+        self.memory.add(state, action, action_probs, reward, next_state, close)
+        torch.save(self.memory, "replayBuffer.pt")
+
+    def backprop_network(self):
+        experience = self.memory.sample()
+        self.learn(experience, gamma)
+
+        actor_path = "actor_state_dict.pt"
+        critic_path = "critic_state_dict_path.pt"
+        torch.save(self.Actor.state_dict(), actor_path)
+        torch.save(self.Criic.state_dict(), critic_path)
+
+        if len(self.memory) % 500 == 0:
+            self.slow_updates(self.Actor, self.Actor_target, self.Critic, self.Critic_target)
 
     def learn(self, experiences, gamma):
-        state, action, action_probs, reward, next_state, close = experiences
+        state, action, action_prob, reward, next_state, close = experiences
         #------------------------critic loss----------------------#
-        _,  actor_target = self.act(state)
+        actor_target = torch.log(self.Actor_target(state))
         Q_target = self.Critic_target(next_state, actor_target)
         state_value = self.Critic(state, action)
 
@@ -88,23 +122,29 @@ class AC_agent():
 
         self.Critic_optimizer.zero_grad()
         critic_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.Critic.parameters(), norm_type=2.0, error_if_nonfinite=True)
         self.Critic_optimizer.step()
 
         #--------------------actor loss--------------------------#
         actor_current = torch.log(self.Actor(state))
         actor_target = self.Critic(next_state, actor_current)
         advantage = Q_value - state_value
-        entropy = -np.sum(np.mean(Q_value) * np.log(Q_value))
-        actor_loss = advantage * -actor_target + entropy + 0.001
+        actor_loss = advantage * -actor_target
 
         self.Actor_optimizer.zero_grad()
         actor_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.Actor.parameters(), norm_type=2.0, error_if_nonfinite=True)
         self.Actor_optimizer.step()
-
-
+        
     def slow_updates(self,actor_current, actor_target, critic_current, critic_target):
         for current, target in zip(actor_current, actor_target):
-            self.Actor.data.copy_(tau*current + (1-tau)*target)
+            target.data.copy_(tau*current + (1-tau)*target)
              
         for current, target in zip(critic_current, critic_target):
-            self.Critic.data.copy_(tau*current + (1-tau)*target)
+            target.data.copy_(tau*current + (1-tau)*target)
+    
+    def script_Models(self):
+        sx = self.env.screenshot()
+        state = self.env.preprocess(sx)
+        actor = self.Actor(state)
+        scripted_Actor = torch.jit.script(actor)
